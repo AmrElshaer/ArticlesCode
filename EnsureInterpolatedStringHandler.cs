6e17823b3a2313f7534a5d3127ca375216e7346b
AmrElshaer <amrelsher07@gmail.com>
2023-07-22 20:34:19 +0300

    Add project files.

---
+++ EnsureInterpolatedStringHandler.cs
@@ -0,0 +1,49 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace TestRider;
+
+[InterpolatedStringHandler]
+public ref struct EnsureInterpolatedStringHandler
+{
+    private DefaultInterpolatedStringHandler _defaultHandler;
+
+    public EnsureInterpolatedStringHandler
+    (
+        int literalLength,
+        int formattedCount,
+        bool condition,
+        out bool shouldAppend
+    )
+    {
+        if (condition)
+        {
+            _defaultHandler = default;
+            shouldAppend = false;
+        }
+
+        _defaultHandler = new DefaultInterpolatedStringHandler(literalLength, formattedCount);
+        shouldAppend = true;
+    }
+
+    public override string ToString()
+    {
+        return _defaultHandler.ToString();
+    }
+
+    public string ToStringAndClear()
+    {
+        return _defaultHandler.ToStringAndClear();
+    }
+
+    public void AppendLiteral(string s)
+    {
+        _defaultHandler.AppendLiteral(s);
+    }
+
+    public void AppendFormatted<T>(T value)
+    {
+        _defaultHandler.AppendFormatted(value);
+    }
+}
+
+
---
+++ LoggingInterpolatedStringHandler.cs
@@ -0,0 +1,41 @@
+﻿// using System.Collections.Generic;
+// using System.Runtime.CompilerServices;
+// using JetBrains.Annotations;
+//
+// namespace TestRider;
+//
+// public ref struct LoggingInterpolatedStringHandler
+// {
+//     private DefaultInterpolatedStringHandler _defaultHandler;
+//
+//     [ItemCanBeNull]
+//     public List<object> Parameters { get; } = new();
+//
+//     public LoggingInterpolatedStringHandler(int literalLength, int formattedCount)
+//     {
+//         _defaultHandler = new DefaultInterpolatedStringHandler(literalLength, formattedCount);
+//     }
+//
+//     public override string ToString()
+//     {
+//         return _defaultHandler.ToString();
+//     }
+//
+//     public string ToStringAndClear()
+//     {
+//         return _defaultHandler.ToStringAndClear();
+//     }
+//
+//     public void AppendLiteral(string s)
+//     {
+//         _defaultHandler.AppendLiteral(s);
+//     }
+//
+//     public void AppendFormatted<T>(T value, [CallerArgumentExpression("value")] string paramName = "")
+//     {
+//         _defaultHandler.AppendFormatted("{" + value + "}");
+//         Parameters.Add(value);
+//     }
+// }
+
+
